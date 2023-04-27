18378b6e0c90eeacf873776b6367cfcde6024142
qualitypedras <programaqualitypedras@gmail.com>
2023-04-26 23:31:38 -0300

    v1

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppLayoutModule } from './layout/app.layout.module';
+import { NotfoundComponent } from './demo/components/notfound/notfound.component';
+import { ProductService } from './demo/service/product.service';
+import { CountryService } from './demo/service/country.service';
+import { CustomerService } from './demo/service/customer.service';
+import { EventService } from './demo/service/event.service';
+import { IconService } from './demo/service/icon.service';
+import { NodeService } from './demo/service/node.service';
+import { PhotoService } from './demo/service/photo.service';
+
+import { AuthService } from './shared/service/auth.service';
+import { MessageService } from 'primeng/api';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+
+@NgModule({
+    declarations: [AppComponent, NotfoundComponent],
+    imports: [
+        AppRoutingModule,
+        AppLayoutModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        AngularFirestoreModule,
+        AngularFireStorageModule,
+        AngularFireDatabaseModule,
+    ],
+    providers: [
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        CountryService,
+        CustomerService,
+        EventService,
+        IconService,
+        NodeService,
+        PhotoService,
+        ProductService,
+        AuthService,
+        MessageService,
+    ],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'programa-bf934',
+    appId: '1:795599374883:web:301d7891c5bbf52a1e3f96',
+    databaseURL: 'https://programa-bf934-default-rtdb.firebaseio.com',
+    storageBucket: 'programa-bf934.appspot.com',
+    apiKey: 'AIzaSyDbVqnapLGCVFXRQJqBFhcNTY6kPhNkJDc',
+    authDomain: 'programa-bf934.firebaseapp.com',
+    messagingSenderId: '795599374883',
+    measurementId: 'G-47LJFDGYLN',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
